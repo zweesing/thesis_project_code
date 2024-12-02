c2b9d53990089af6161cb8ec0749cb9e210711e1
Ziggy <ziggy.weesing@gmail.com>
2024-12-02 16:50:23 +0100

    Added GRF particle making, including optional porosity. does not output as an array yet, and no mantle function yet.

--- make_GRF.py
+++ make_GRF.py
@@ -2,47 +2,119 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import argparse
+import time
+from copy import deepcopy
 
 
 def plot3d(arr):
     fig = plt.figure()
     ax = fig.add_subplot(projection="3d")
 
-    ax.scatter(arr[:, 0], arr[:, 1], arr[:, 2])
+    # find the coordinates for plotting
+    x, y, z = np.where(arr == 1)
 
-    ax.set_xlabel("i")
-    ax.set_ylabel("j")
-    ax.set_zlabel("k")
+    # https://discourse.matplotlib.org/t/collection-of-markers-with-size-set-in-data-units/21057/2
+    # this has a good idea on how to make the plotting look nice i think. not implemented yet
 
+    # Plot the points
+    ax.scatter(x, y, z, marker="s")
+
+    ax.set_xlabel("X")
+    ax.set_ylabel("Y")
+    ax.set_zlabel("Z")
     ax.set_aspect("equal")
+    ax.set_title(f"M={M}, rho={rho}")
 
     plt.show()
 
 
+# for testing
+# np.random.seed(10)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("M", help="number of points", default=40, type=int, nargs="?")
+parser.add_argument("rho", help="size", default=1, type=int, nargs="?")
+args = parser.parse_args()
+
+# ---------------------------------------------------------------------------------- #
+# variables
+# ---------------------------------------------------------------------------------- #
+
 # number of points
-M = 4
+M = args.M
 # size of particles
-rho = 1
+rho = args.rho
+# threshold value
+threshold = 0.5
+# porosity division (rho' = rho/por_div). The effect of this is very dependent om M of course.
+# it does not scale linearly.
+por_div = 50
+# porosity threshold
+por_threshold = 0.2
+# add porosity or not
+porosity = False
+# ---------------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------------- #
 
 # construct random points
-Rijk = np.random.rand(M, 3)
+print("constructing GRF...")
+start_time = time.time()
 
+Rijk = np.random.rand(M, M, M)
 
-# distances to the center
+# distances to the center 3d array. this is actually half a point off center? but it maches the algorithm.
+i_arr = np.arange(M) + 1
+j_arr = np.arange(M) + 1
+k_arr = np.arange(M) + 1
+
+print("making distances array...")
 d = np.sqrt(
-    (Rijk[:, 0] - M / 2) ** 2 + (Rijk[:, 1] - M / 2) ** 2 + (Rijk[:, 2] - M / 2) ** 2
+    (i_arr[:, np.newaxis, np.newaxis] - M / 2) ** 2
+    + (j_arr[:, np.newaxis] - M / 2) ** 2
+    + (k_arr - M / 2) ** 2
 )
 
 # construct the field
-Gijk = Rijk * np.exp(-rho * d[:, None] ** 2)
+Gijk = Rijk * np.exp(-rho * d**2)
 
 # we need to take the 3d fourier transform. ( not sure how this function wants the input to look like and what axes to specify) (axes=[0,1] or [1,0] does not change anything)
-GijkF = np.fft.fftn(Gijk, axes=[0, 1])
-print(GijkF)
-print(" ")
-print(np.fft.fftn(Gijk))
-# normalise
-GijkF = GijkF / np.max(GijkF, 1)[:, None]
+GijkF = np.fft.fftn(Gijk)
 
-# all points where GijF is > 0.5 are inside the particle. this Is unclear because the fourier transform does not return single value per point, but the transformed coordinates (complex).
-# do I take the length of the fourier transformed vector?
+print("first fourier...")
+
+# normalise
+GijkF = GijkF / np.max(GijkF)
+
+# all points where GijF is > 0.5 are inside the particle.
+# so all points in space will be evaluated besed on the value from Gijkf
+space = np.zeros((M, M, M))
+space[GijkF > threshold] = 1
+
+no_poros = deepcopy(space)
+print("done.")
+print(np.count_nonzero(space))
+# ---------------------------------------------------------------------------------- #
+# we can add porosity
+# ---------------------------------------------------------------------------------- #
+print("adding porosity...")
+if porosity:
+    # second GRF with smaller effective size
+    rho2 = rho / por_div
+
+    # construct the field
+    Gijk2 = Rijk * np.exp(-rho2 * d**2)
+    print("second fourier...")
+    GijkF2 = np.fft.fftn(Gijk2)
+    print("done with second fourier.")
+    GijkF2 = GijkF2 / np.max(GijkF2)
+
+    # we add vacuum back where gijkf2 is above the porosity threshold
+    space[GijkF2 > por_threshold] = 0
+print(np.count_nonzero(space))
+
+# see where the porosity got added.
+por_diff = no_poros - space  # this should leave 1 values where there is stuff removed
+
+print(np.count_nonzero(por_diff))
+plot3d(space)
