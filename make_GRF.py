59ed7b2f1ca5281c9a9e642532888c6115505315
Ziggy <ziggy.weesing@gmail.com>
2024-12-10 15:00:26 +0100

    now extracts all the particles that are created, with a recursive neighbour search

--- make_GRF.py
+++ make_GRF.py
@@ -4,10 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 import time
-from copy import deepcopy
+import copy
 
 
-def plot3d(arr):
+def plot3d(arr, title=None):
     fig = plt.figure()
     ax = fig.add_subplot(projection="3d")
 
@@ -23,8 +23,15 @@ def plot3d(arr):
     ax.set_xlabel("X")
     ax.set_ylabel("Y")
     ax.set_zlabel("Z")
+    print(arr.shape)
+    ax.set_xlim(0, arr.shape[0])
+    ax.set_ylim(0, arr.shape[1])
+    ax.set_zlim(0, arr.shape[2])
+
     ax.set_aspect("equal")
     ax.set_title(f"M={M}, rho={rho}")
+    if title:
+        ax.set_title(title)
 
     plt.show()
 
@@ -102,21 +109,20 @@ GijkF = GijkF / np.max(GijkF)
 space = np.zeros((M, M, M))
 space[GijkF > threshold] = 1
 
-no_poros = deepcopy(space)
+no_poros = copy.copy(space)
 print("done.")
 print(np.count_nonzero(space))
 
 # ---------------------------------------------------------------------------------- #
-# determine boundaries
-# can just do this with a neighbour search but its gonna be hard and long
+# extract the particles from here
 # ---------------------------------------------------------------------------------- #
-plot_slice(space[0])
+# plot_slice(space[0])
 
 # pick a point. any point that is inside a particle.
 # check all eight neighbours. if any are outside the particle, add point to some list
 #  go to new point that is one of those neighbours and repeat
 
-# what does this give us? in the end, an entire outside boundary of the particle (potato peel)
+# what does this give us? in the end, a0n entire outside boundary of the particle (potato peel)
 # this means we have the 6 bounds to extract it.
 
 # check the ouside of the extraction box for particle bits (ones) (jonas' idea).
@@ -127,6 +133,73 @@ plot_slice(space[0])
 # could also just try neighbour searching for the entire thing.
 # recursively. picture of cris' pseudocode
 
+
+# this method should work after dealing with porosity
+def get_particle(coordinate, arr, destination_array):
+    """given a coordinate, check if the coordinate is inside a particle in the 3d array.
+    then recursively check all the neighbouring spaces to get all the dipoles of a particle.
+    works for one particle,and has to start within a particle.
+
+    Args:
+        coordinate (tuple): x,y,z coordinate of starting point
+        arr (3D array): space array containing all the particles
+        destination_array
+    """
+    # get the 26 neighbours. check if they are not outside of the array
+    x, y, z = coordinate
+    neighbours = []
+    for xi in [-1, 0, 1]:
+        for yi in [-1, 0, 1]:
+            for zi in [-1, 0, 1]:
+                if (
+                    not (xi == 0 and yi == 0 and zi == 0)
+                    and (0 <= x + xi < M)
+                    and (0 <= y + yi < M)
+                    and (0 <= z + zi < M)
+                ):  # don't add the current coordinate, and dont add anything outside of the array
+                    neighbours.append((x + xi, y + yi, z + zi))
+
+    # save point and remove from original
+    destination_array[coordinate] = 1
+    arr[coordinate] = 0
+
+    # for each neighbour, call function and then add to particle and remove from original
+    for neighbour in neighbours:
+        if arr[neighbour] == 1:
+            # if its inside the particle, go deeper.
+            get_particle(neighbour, arr, particle_arr)
+
+
+# we will do this for each particle so there will be a while loop involved.
+particles_removed = False
+space_copy = copy.copy(space)  # space_copy will be modified an dslowly eaten
+particles = []  # list to fill with particles arrays
+
+# get a point inside a particle. this can be done a lot easier possibly but this works
+x, y, z = np.where(space_copy == 1)
+coordinate = (x[0], y[0], z[0])
+
+while not particles_removed:
+
+    x, y, z = np.where(space_copy == 1)
+    coordinate = (x[0], y[0], z[0])
+
+    particle_arr = np.zeros((M, M, M))
+    get_particle(coordinate, space_copy, particle_arr)
+
+    particles.append(particle_arr)
+
+    # new particle
+    x, y, z = np.where(space_copy == 1)
+    if x.size > 0:
+        coordinate = (x[0], y[0], z[0])
+    else:
+        particles_removed = True
+
+num_parts = len(particles)
+print("number of particles removed:", num_parts)
+plot3d(space_copy, title="(hopefully) empty space")
+plot3d(space, title=f"space with hopefully {num_parts} particles")
 
 # ---------------------------------------------------------------------------------- #
 # we can add porosity
@@ -145,12 +218,13 @@ if porosity:
 
     # we add vacuum back where gijkf2 is above the porosity threshold
     space[GijkF2 > por_threshold] = 0
-print(np.count_nonzero(space))
 
-# see where the porosity got added.
-por_diff = no_poros - space  # this should leave 1 values where there is stuff removed
+    # see where the porosity got added.
+    por_diff = (
+        no_poros - space
+    )  # this should leave 1 values where there is stuff removed
 
-print(np.count_nonzero(por_diff))
+
 plot3d(space)
 
 # ---------------------------------------------------------------------------------- #
